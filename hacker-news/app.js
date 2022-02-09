66c6e5994427bc802c13aaa378483e2e78c92eed
beomseok <bmtosss@gmail.com>
2022-02-09 17:01:48 +0900

    FEAT: 해시를 이용한 클릭 타이틀 표시

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -1,24 +1,32 @@
+const container = document.getElementById('root');
+const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = `https://hacker-news.firebaseio.com/v0/item/@id.json`;
+
 const ajax = new XMLHttpRequest();
-ajax.open(
-  'GET',
-  'https://hacker-news.firebaseio.com/v0/topstories.json',
-  false
-);
+ajax.open('GET', NEWS_URL, false);
 ajax.send();
-const numbers = JSON.parse(ajax.response).slice(0, 10);
+
+const newsFeeds = JSON.parse(ajax.response).slice(0, 10);
 
 const ul = document.createElement('ul');
-document.getElementById('root').appendChild(ul);
+const content = document.createElement('div');
 
-numbers.forEach((number) => {
-  const li = document.createElement('li');
-  ajax.open(
-    'GET',
-    `https://hacker-news.firebaseio.com/v0/item/${number}.json?print=pretty`,
-    false
-  );
+window.addEventListener('hashchange', () => {
+  const id = location.hash.substr(1);
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
   ajax.send();
-  const item = JSON.parse(ajax.response);
-  li.innerHTML = item.title;
+  const title = JSON.parse(ajax.response).title;
+  content.innerHTML = title;
+});
+
+newsFeeds.forEach((newsFeed) => {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  a.innerHTML = newsFeed.title;
+  a.href = `#${newsFeed.id}`;
+  li.appendChild(a);
   ul.appendChild(li);
 });
+
+container.appendChild(ul);
+container.appendChild(content);
