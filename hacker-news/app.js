06689e17482a52a2add045e9502b9a01c8b387e4
beomseok <bmtosss@gmail.com>
2022-02-10 10:49:44 +0900

    FEAT: 라우터 설정

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -3,37 +3,49 @@ const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = `https://hacker-news.firebaseio.com/v0/item/@id.json`;
 
 const ajax = new XMLHttpRequest();
+
 const getData = (url) => {
   ajax.open('GET', url, false);
   ajax.send();
   return JSON.parse(ajax.response);
 };
 
-const newsFeeds = getData(NEWS_URL).slice(0, 10);
+const showNewsFeeds = () => {
+  const newsFeeds = getData(NEWS_URL).slice(0, 10);
+  const newsList = [];
+  newsList.push('<ul>');
 
-const ul = document.createElement('ul');
+  newsFeeds.forEach((newsFeed) => {
+    newsList.push(`
+    <li>
+    <a href='#${newsFeed.id}'>${newsFeed.title}(${newsFeed.comments_count})</a>
+    </li>
+    `);
+  });
+  newsList.push('</ul>');
 
-window.addEventListener('hashchange', () => {
+  container.innerHTML = newsList.join('');
+};
+
+const showNewsDetail = () => {
   const id = location.hash.substr(1);
   const title = getData(CONTENT_URL.replace('@id', id)).title;
   container.innerHTML = `
-    <h1>${title}</h1>
-
-    <div><a href='#'>돌아가기</a></div>
+  <h1>${title}</h1>
+  
+  <div><a href='#'>돌아가기</a></div>
   `;
-});
+};
 
-const newsList = [];
-newsList.push('<ul>');
+const router = () => {
+  const routePath = location.hash;
+  if (routePath === '') {
+    showNewsFeeds();
+  } else {
+    showNewsDetail();
+  }
+};
 
-newsFeeds.forEach((newsFeed) => {
-  newsList.push(`
-    <li>
-      <a href='#${newsFeed.id}'>${newsFeed.title}(${newsFeed.comments_count})</a>
-    </li>
-  `);
-});
-newsList.push('</ul>');
+window.addEventListener('hashchange', router);
 
-container.innerHTML = newsList.join('');
-container.appendChild(content);
+router();
