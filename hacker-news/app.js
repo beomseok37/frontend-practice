75e0a02da6e1de3eb8d171c2b2051f396b3eee5b
beomseok <bmtosss@gmail.com>
2022-02-12 12:12:44 +0900

    FEAT: 페이징 구현

--- hacker-news/app.js
+++ hacker-news/app.js
@@ -1,6 +1,9 @@
 const container = document.getElementById('root');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = `https://hacker-news.firebaseio.com/v0/item/@id.json`;
+const store = {
+  currentPage: 1,
+};
 
 const ajax = new XMLHttpRequest();
 
@@ -9,37 +12,56 @@ const getData = (url) => {
   ajax.send();
   return JSON.parse(ajax.response);
 };
-
+const datas = getData(NEWS_URL);
+console.log(datas.length);
 const showNewsFeeds = () => {
-  const newsFeeds = getData(NEWS_URL).slice(0, 10);
+  console.log(store.currentPage);
+  const newsFeeds = datas.slice(
+    (store.currentPage - 1) * 10,
+    store.currentPage * 10
+  );
   const newsList = [];
   newsList.push('<ul>');
 
   newsFeeds.forEach((newsFeed) => {
     newsList.push(`
     <li>
-    <a href='#${newsFeed.id}'>${newsFeed.title}(${newsFeed.comments_count})</a>
+      <a href='#/show/${newsFeed.id}'>${newsFeed.title}(${newsFeed.comments_count})</a>
     </li>
     `);
   });
   newsList.push('</ul>');
-
+  newsList.push(`
+    <div>
+      <a href='#/page/${
+        store.currentPage === 1 ? 1 : store.currentPage - 1
+      }'>이전 페이지</a>
+      <a href='#/page/${
+        store.currentPage === Number(datas.length / 10)
+          ? Number(datas.length / 10)
+          : store.currentPage + 1
+      }'>다음 페이지</a>
+    </div>
+  `);
   container.innerHTML = newsList.join('');
 };
 
 const showNewsDetail = () => {
-  const id = location.hash.substr(1);
+  const id = location.hash.substr(7);
   const title = getData(CONTENT_URL.replace('@id', id)).title;
   container.innerHTML = `
   <h1>${title}</h1>
-  
-  <div><a href='#'>돌아가기</a></div>
+
+  <div><a href='#/page/${store.currentPage}'>돌아가기</a></div>
   `;
 };
 
 const router = () => {
   const routePath = location.hash;
   if (routePath === '') {
+    showNewsFeeds();
+  } else if (routePath.includes('page')) {
+    store.currentPage = Number(location.hash.substr(7));
     showNewsFeeds();
   } else {
     showNewsDetail();
