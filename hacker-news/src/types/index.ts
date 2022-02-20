43b213a20389b50dd46873f7b79718da3ff6b383
beomseok <bmtosss@gmail.com>
2022-02-20 21:04:26 +0900

    CHORE: 파일 구조 리팩터링

---
+++ hacker-news/src/types/index.ts
@@ -0,0 +1,34 @@
+import View from '../core/view';
+
+export type Store = {
+  currentPage: number;
+  feeds: NewsFeed[];
+};
+
+export type News = {
+  id: number;
+  time_ago: string;
+  title: string;
+  url: string;
+  user: string;
+  content: string;
+};
+export type NewsFeed = News & {
+  comments_count: number;
+  points: number;
+  read?: boolean;
+};
+
+export type NewsDetail = News & {
+  comments: NewsComment[];
+};
+
+export type NewsComment = News & {
+  comments: NewsComment[];
+  level: number;
+};
+
+export type RouteInfo = {
+  path: string;
+  page: View;
+};
